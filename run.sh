a03dc4f8257ae8ac726becaa21753ca46ce19cde
David Chapdelaine <david.chapdelaine@gmail.com>
2018-10-01 13:02:50 -0400

    initial work on call to azure rm api

--- run.sh
+++ run.sh
@@ -21,3 +21,8 @@ az group deployment create \
   --parameters publisherEmail=email@example.com \
   --parameters publisherName=templateTest \
   --parameters subnetId=$subnetId
+
+  token=$(az account get-access-token --query accessToken | xargs)
+  curl -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" https://management.azure.com/subscriptions/[SUBSCRIPTION_ID]/providers/Microsoft.Web/sites?api-version=2016-08-01
+
+  GET https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ApiManagement/service/{serviceName}/tenant/access/git?api-version=2018-06-01-preview
