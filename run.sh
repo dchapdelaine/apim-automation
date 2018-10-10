678bc2905c846b5ad37e8b92ebab771fdeda3d28
David Chapdelaine <david.chapdelaine@gmail.com>
2018-10-10 17:07:29 -0400

    work in progress

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 rgName="apim"
+apimName="apiserviceo5xmu3coaq5m2"
 vnetName="apimvnet"
 subnetName="apimsubnet"
 location="Canada East"
@@ -13,6 +14,7 @@ az group create --name $rgName --location $location
 az network vnet create -g $rgName --subnet-name $subnetName -n $vnetName
 # xargs strips double quotes
 subnetId=$(az network vnet subnet show -g $rgName -n $subnetName --vnet-name $vnetName --query "id" | xargs)
+subId=$(az account show --query "id" | xargs)
 
 az group deployment create \
   --name deployment \
@@ -20,9 +22,8 @@ az group deployment create \
   --template-file azuredeploy.json \
   --parameters publisherEmail=email@example.com \
   --parameters publisherName=templateTest \
-  --parameters subnetId=$subnetId
+  --parameters subnetId=$subnetId \
+  --parameters apiManagementServiceName=$apimName
 
   token=$(az account get-access-token --query accessToken | xargs)
-  curl -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" https://management.azure.com/subscriptions/[SUBSCRIPTION_ID]/providers/Microsoft.Web/sites?api-version=2016-08-01
-
-  GET https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ApiManagement/service/{serviceName}/tenant/access/git?api-version=2018-06-01-preview
+  curl -X GET -H "Authorization: Bearer $token" -H "Content-Type: application/json" https://management.azure.com/subscriptions/$subId/resourceGroups/$rgName/providers/Microsoft.ApiManagement/service/$apimName/tenant/access/git -d "api-version=2018-06-01-preview" 
